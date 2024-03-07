5f3281e9488f22b8c678bed56797d4a2fedce3b4
Thallyson dias <thallysondias@gmail.com>
2024-03-07 11:38:18 +0000

    update package and chart load

--- app/admin/participations/components/Overview.tsx
+++ app/admin/participations/components/Overview.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import {
   RecentRegisterProps,
   EntityQuestion,
@@ -22,10 +23,15 @@ import {
   YAxis,
   Tooltip,
   Legend,
-  CartesianGrid,
 } from "recharts";
 
 import { questions } from "@/lib/questions";
+
+interface ChartData {
+  name: string | number;
+  Participações: number;
+  Interesse: number;
+}
 
 const groupUsersByEntity = (
   users: UserData[]
@@ -96,26 +102,38 @@ const usersWantEventByEntity = (
 export default function OverviewParticipation({
   dataUser,
 }: RecentRegisterProps) {
+  const [chartData, setChartData] = useState<ChartData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const usersGroupedByEntity: { [entityValue: number]: UserData[] } =
     groupUsersByEntity(dataUser.items);
   const usersWantEvent = usersWantEventByEntity(dataUser.items);
 
   // Preparar os dados para o PieChart
-  const chartData = Object.entries(usersGroupedByEntity).map(
-    ([entityId, entityUsers]) => {
-      const entityName =
-        questions.find((q) => q.entity_id === parseInt(entityId))?.entity ||
-        "Unknown Entity";
-      const numberOfParticipants = entityUsers.length;
-      const numberOfParticipantsWantEvent =
-        usersWantEvent[parseInt(entityId)] || 0;
-      return {
-        name: entityName,
-        Participações: numberOfParticipants,
-        Interesse: numberOfParticipantsWantEvent,
-      };
-    }
-  );
+  useEffect(() => {
+    const usersGroupedByEntity = groupUsersByEntity(dataUser.items);
+    const usersWantEvent = usersWantEventByEntity(dataUser.items);
+
+    // Preparar os dados para o gráfico
+    const data = Object.entries(usersGroupedByEntity).map(
+      ([entityId, entityUsers]) => {
+        const entityName =
+          questions.find((q) => q.entity_id === parseInt(entityId))?.entity ||
+          "Unknown Entity";
+        const numberOfParticipants = entityUsers.length;
+        const numberOfParticipantsWantEvent =
+          usersWantEvent[parseInt(entityId)] || 0;
+        return {
+          name: entityName,
+          Participações: numberOfParticipants,
+          Interesse: numberOfParticipantsWantEvent,
+        };
+      }
+    );
+
+    setChartData(data); // Atualiza os dados do gráfico
+    setIsLoading(false); // Atualiza o estado de carregamento
+  }, [dataUser]); // Dependência do useEffect
 
   return (
     <>
@@ -124,35 +142,62 @@ export default function OverviewParticipation({
           <CardTitle>Participações Gerais</CardTitle>
         </CardHeader>
         <CardContent className="pl-2">
-          <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={chartData} 
-            margin={{
-              top: 15,
-            }}>
-              <XAxis
-                dataKey="name"
-                fontSize={12}
-                tickLine={false}
-                axisLine={false}                
-              />
-              <YAxis />
-              <Bar
-                dataKey="Participações"
-                fill="currentColor"
-                className="fill-primary"
-                radius={[4, 4, 0, 0]}
-                label={{ position: "top", fontSize:11 }}
-              />
-              <Bar
-                dataKey="Interesse"
-                fill="#008f3e"
-                radius={[4, 4, 0, 0]}
-                label={{ position: "top", fontSize:11 }}
-              />
-              <Tooltip />
-              <Legend wrapperStyle={{fontSize: "10px"}} />
-            </BarChart>
-          </ResponsiveContainer>
+          {isLoading ? ( // Verifica o estado de carregamento
+           <div className="animate-pulse">
+           <div className="flex space-x-4 items-baseline m-6">
+             <div className="flex-1 h-24 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-36 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-48 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-64 bg-gray-300 rounded"></div>
+             <div className="flex-1 h-72 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-64 bg-gray-300 rounded"></div>
+             <div className="flex-1 h-48 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-36 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-72 bg-gray-300 rounded"></div>
+             <div className="flex-1 h-36 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-48 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-64 bg-gray-300 rounded"></div>
+             <div className="flex-1 h-72 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-64 bg-gray-300 rounded"></div>
+             <div className="flex-1 h-48 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-36 bg-gray-200 rounded"></div>
+             <div className="flex-1 h-24 bg-gray-200 rounded"></div>
+           </div>
+         </div>
+           // Exibe um texto ou componente de carregamento
+          ) : (
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart
+                data={chartData}
+                margin={{
+                  top: 15,
+                }}
+              >
+                <XAxis
+                  dataKey="name"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <YAxis />
+                <Bar
+                  dataKey="Participações"
+                  fill="currentColor"
+                  className="fill-primary"
+                  radius={[4, 4, 0, 0]}
+                  label={{ position: "top", fontSize: 11 }}
+                />
+                <Bar
+                  dataKey="Interesse"
+                  fill="#008f3e"
+                  radius={[4, 4, 0, 0]}
+                  label={{ position: "top", fontSize: 11 }}
+                />
+                <Tooltip />
+                <Legend wrapperStyle={{ fontSize: "10px" }} />
+              </BarChart>
+            </ResponsiveContainer>
+          )}
         </CardContent>
       </Card>
     </>
